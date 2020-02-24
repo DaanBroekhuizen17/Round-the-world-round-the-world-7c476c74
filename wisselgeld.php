b46921d46e043d984ed9e26b3e8777dd4f1365a0
daan <2087884@talnet.nl>
2020-02-24 12:23:47 +0100

    het zelfde als vorige

---
+++ wisselgeld.php
@@ -0,0 +1,36 @@
+<?php
+
+$input = $argv[1];
+$bedrag = floatval($input);
+
+define("geldeenheden", [
+    500,
+    200,
+    100,
+    50,
+    20,
+    10,
+    5,
+]);
+
+
+$restbedrag = $bedrag;
+
+foreach(geldeenheden as $euro){
+    if($restbedrag >= $euro){
+        $aantalKeerEuroInRestBedrag = floor($restbedrag / $euro);
+        $restbedrag = $restbedrag - $euro * $aantalKeerEuroInRestBedrag;
+        echo($aantalKeerEuroInRestBedrag. " X " .$euro. " euro".PHP_EOL);
+    }
+}
+
+$restbedrag = $restbedrag * 100;
+
+foreach(geldeenheden as $euro){
+    if($restbedrag >= $euro){
+        $aantalKeerEuroInRestBedrag = floor($restbedrag / $euro);
+        $restbedrag = round($restbedrag - $euro * $aantalKeerEuroInRestBedrag);
+        echo($aantalKeerEuroInRestBedrag. " X " .$euro. " cent".PHP_EOL);
+    }
+}
+?>
